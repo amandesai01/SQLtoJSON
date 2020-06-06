9defdae4c33e9aa0e4db2ff4e366db8ae0bdc65e
Aman Desai <amandesai01@gmail.com>
2020-06-06 12:09:41 +0530

    History Integrated

--- pypi/s2j.py
+++ pypi/s2j.py
@@ -16,6 +16,7 @@ class s2j:
         self.__host = host
         self.__database = database
         self.__authenticate(host, username, password, database)
+        self.__history = []
 
     def __authenticate(self, username, password):
         data = { 'username': username , 'password': password }
@@ -29,7 +30,16 @@ class s2j:
         data = {'query' : query}
         headers = {'token' : self.__header}
         rawres = requests.get("https://127.0.0.1:4909/execQuery", json = data, headers = headers).json()
+        history_element = {}
+        history_element['query'] = query
+        history_element['success'] = False
+        history_element['response'] = None
+        history_element['error'] = None
         if rawres.get['status'] == "failure":  
             print(rawres.get("error_message"))
+            history_element['error'] = rawres.get("error_message")
+            self.__history.append(history_element)
             return None
+        history_element['success'] = True
+        history_element['response'] = rawres.get("data")
         return rawres

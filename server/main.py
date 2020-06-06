5942026b421a09880ea8e98e6f077b082b5bbe6e
Aman Desai <amandesai01@gmail.com>
2020-06-06 11:41:55 +0530

    Deprecated Dumping Protocol

--- server/main.py
+++ server/main.py
@@ -4,40 +4,6 @@ import requests
 from cruds import query
 app = Flask(__name__)
 
-@app.route('/dumpcreds', methods = ['GET'])
-def dumpCreds():
-    cred = {}
-    url = request.args.get('url')
-    port = request.args.get('port')
-    username = request.args.get('username')
-    password = request.args.get('password')
-    if url is None or port is None or username is None or password is None:
-        return jsonify({"status" : "error"})
-    cred['url'] = url
-    cred['port'] = port
-    cred['username'] = username
-    cred['password'] = password
-    
-    try:
-        with open('config.json', 'w') as fp:
-            try:
-                json.dump(cred, fp, indent=4)
-                return jsonify({"status" : "ok"})
-            except Exception as e:
-                return jsonify({"status" : "error"})
-    except:
-        return jsonify({"status" : "error"})
-    
-@app.route('/checkcreds')
-def checkCreds():
-    try:
-        with open('config.json', 'r') as fp:
-            data = json.load(fp)
-            data['status'] = "ok"
-            return jsonify(data)
-    except:
-        return jsonify({ 'status': 'error' })
-
 @app.route('/execQuery')
 def execQuery():
     database = request.args.get('database')

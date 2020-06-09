1721a981f5c311aee0b6596d9055b3c7c4d8aba3
Aman Desai <amandesai01@gmail.com>
2020-06-09 10:37:39 +0530

    Check Connection route ready

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 import requests
-from cruds.query import exec_query
+from cruds.query import exec_query, checkConnection
 app = Flask(__name__)
 
 @app.route('/execQuery', methods = ["POST", "GET"])
@@ -17,6 +17,17 @@ def execQuery():
         return jsonify(exec_query(query, database, host, username, password))
     except Exception as e:
         return jsonify({ "status" : "failure", "error_message" : str(e) })
+
+@app.route('/check')
+def check():
+    data = request.get_json()
+    database = data.get('database')
+    host = data.get('host')
+    username = data.get('username')
+    password = data.get('password')
+    if checkConnection(database, host, username, password):
+        return jsonify({"status" : "ok"})
+    return jsonify({"status" : "failure"})
 
 if __name__ == "__main__":
     app.run(port = 4909, debug = True)

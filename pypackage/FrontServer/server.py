f970801df7467dd7426fe983d2a5f564b441ac5c
Aman Desai <amandesai01@gmail.com>
2020-06-10 13:57:31 +0530

    Revert "fixes"
    This reverts commit c252c01f95cd2b5650387d32cb4dacaaab5c5d0a.

--- pypackage/FrontServer/server.py
+++ pypackage/FrontServer/server.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, session, url_for, redirect
+from flask import Flask, render_template
 import pickle
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 
 app = Flask(__name__)
 app.secret_key = "KKKKEEEEYYY"
-app.config['pickle'] = None
 # s2jObj = None
 class queryForm(FlaskForm):
     Query = StringField()
@@ -22,7 +21,7 @@ class newConnForm(FlaskForm):
 def new():
     form = newConnForm()
     if form.is_submitted():
-        s2jObj = pickle.dumps(app.config['pickle'])
+        backupS2JInstance =pickle.dumps(s2jObj)
         s2jObj.updateHost(form.host.data)
         s2jObj.updateUsername(form.username.data)
         s2jObj.updatePassword(form.password.data)
@@ -31,42 +30,34 @@ def new():
             return render_template('index.html')
         else:
             #restoring old version
+            s2jObj = pickle.loads(backupS2JInstance)
             return render_template('newconnection.html', form = form, error=True)
     return render_template('newconnection.html', form = form, error=False)
 
 @app.route('/querytab', methods=["POST", "GET"])
 def querytab():
-    if app.config['pickle']:
-        s2jObj = pickle.loads(app.config['pickle'])
-        form = queryForm()
-        if form.is_submitted():
-            query = form.Query.data
-            resp = s2jObj.execQuery(query)
-            return render_template('try_query.html', form=form, isRes=True, res=resp)
-        return render_template('try_query.html', form=form, isRes=False, res="")
-    else:
-        return redirect(url_for('new'))
+    form = queryForm()
+    if form.is_submitted():
+        query = form.Query.data
+        resp = s2jObj.execQuery(query)
+        return render_template('try_query.html', form=form, isRes=True, res=resp)
+    return render_template('try_query.html', form=form, isRes=False, res="")
 
 @app.route("/history")
 def history():
     # print(s2jObj.get_history())
-    if app.config['pickle']:
-        s2jObj = pickle.loads(app.config['pickle'])
-        return render_template('history.html', history = s2jObj.get_history())
-    else:
-        return redirect(url_for('new'))
+    return render_template('history.html', history = s2jObj.get_history())
 
 @app.route("/")
 def index():
-    if app.config['pickle']:
-        # print(s2jObj.get_history())
-        return render_template('index.html')
-    else:
-        return redirect(url_for('new'))
+    # print(s2jObj.get_history())
+    return render_template('index.html')
 
 def start(pick):
-    app.config['pickle'] = pick
-    if app.config['pickle']: # This means that object was successfully setuped in session.
+    global s2jObj
+    s2jObj = pickle.loads(pick)
+    print(s2jObj)
+    if s2jObj:
         app.run(port=4001)
 
 def update_pickle(pick):
